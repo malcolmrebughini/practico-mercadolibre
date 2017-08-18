acd6fac2756bb8a68559e6f4cd3c6ba2b476542e
Malcolm <malcolm.rebughini@gmail.com>
2017-08-17 22:39:35 -0300

    refactor(services/mercadolibre, config/environment): Mercadolibre api url via env var

--- server/services/mercadolibre.js
+++ server/services/mercadolibre.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
+const config = require('../config/environment');
 
-const apiUrl = 'https://api.mercadolibre.com';
+
 const searchUrl = '/sites/MLA/search';
 const categoryUrl = '/categories/:id';
 const itemUrl = '/items/:id';
@@ -8,22 +9,22 @@ const itemDescriptionUrl = `${itemUrl}/description`;
 
 
 function search(query) {
-  return axios.get(`${apiUrl}${searchUrl}`, { params: { q: query, limit: 4 } })
+  return axios.get(`${config.mercadolibreApiUrl}${searchUrl}`, { params: { q: query, limit: 4 } })
     .then(response => response.data);
 }
 
 function getCategory(categoryId) {
-  return axios.get(`${apiUrl}${categoryUrl.replace(':id', categoryId)}`)
+  return axios.get(`${config.mercadolibreApiUrl}${categoryUrl.replace(':id', categoryId)}`)
     .then(response => response.data);
 }
 
 function getItem(itemId) {
-  return axios.get(`${apiUrl}${itemUrl.replace(':id', itemId)}`)
+  return axios.get(`${config.mercadolibreApiUrl}${itemUrl.replace(':id', itemId)}`)
     .then(response => response.data);
 }
 
 function getItemDescription(itemId) {
-  return axios.get(`${apiUrl}${itemDescriptionUrl.replace(':id', itemId)}`)
+  return axios.get(`${config.mercadolibreApiUrl}${itemDescriptionUrl.replace(':id', itemId)}`)
     .then(response => response.data);
 }
 
