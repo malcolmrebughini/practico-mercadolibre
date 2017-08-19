7f11e4178cd622849b7df47a8d9b28f1501e670e
Malcolm <malcolm.rebughini@gmail.com>
2017-08-18 23:18:32 -0300

    test(Breadcrumbs): Adding Breadcrumbs snapshot test

--- app/components/Breadcrumbs/Breadcrumbs.spec.js
+++ app/components/Breadcrumbs/Breadcrumbs.spec.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import Breadcrumbs from './Breadcrumbs';
 import styles from './styles.scss';
 
+const testKeywordsList = ['this', 'are', 'some', 'breadcrumbs', 'mydude'];
 
 describe('Breadcrumbs', () => {
+  it('should render correctly', () => {
+    const tree = renderer.create(
+      <Breadcrumbs list={testKeywordsList} />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
   it('should render a list of strings as breadcrumbs', () => {
-    const keywordsList = ['this', 'are', 'some', 'breadcrumbs', 'mydude'];
+
     const wrapper = shallow(
-      <Breadcrumbs list={keywordsList} />
+      <Breadcrumbs list={testKeywordsList} />
     );
 
     const breadcrumbs = wrapper.find(`.${styles.breadCrumb}`);
@@ -16,7 +26,7 @@ describe('Breadcrumbs', () => {
     expect(breadcrumbs.length).toEqual(5);
 
     breadcrumbs.forEach((breadcrumb, index) => {
-      expect(breadcrumb.text()).toContain(keywordsList[index]);
+      expect(breadcrumb.text()).toContain(testKeywordsList[index]);
     });
   });
 });
